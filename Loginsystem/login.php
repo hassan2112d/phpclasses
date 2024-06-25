1c22ed9f48242efd8f03f7fff5ec955959847eed
hassan2112d <hassan2112d@aptechgdn.net>
2024-06-26 00:29:47 +0500

    Login compelete

--- Loginsystem/login.php
+++ Loginsystem/login.php
@@ -2,27 +2,23 @@
 <div class="container">
 <h1>Welcome Login</h1>
 
-<form>
+<form action="login.php" method="post">
   <div class="form-group ">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="exampleInputEmail1">Email :</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="exampleInputPassword1">Password :</label>
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <button type="submit" name="submit" value="login" class="btn btn-primary">Submit</button>
 </form>
 </div>
 
-<?php 
+ 
 
-// $_SESSION :  Is much secure than cookie and its store in the cache data .
+<!-- // $_SESSION :  Is much secure than cookie and its store in the cache data .
 
 
 
@@ -38,7 +34,37 @@
 // session_start();
 // session_destroy();
 // session_unset();
+ -->
+<?php 
+session_start();
 
+if(isset($_POST['submit'])) {
 
+    include '../Loginsystem/db_connect.php';
 
+    $useremail = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM `users` WHERE user_email = '$useremail'";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0) {
+
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row['user_pass'])) {
+               
+            $_SESSION['useremail'] = $useremail;
+
+            header("Location: Welcomepage.php");
+            exit(); // Always use exit after header redirection
+        } else {
+            echo "Invalid password.";
+        }
+
+    } else {
+        echo "No user found with that username.";
+    }
+}
 ?>
+
+
